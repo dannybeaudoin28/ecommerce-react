4d1c5590471d53bfe9e5dd39d56877554d189dc2
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2023-09-01 10:15:25 -0400

    Add basic user signup page

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,38 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import { 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         const response = await getRedirectResult(auth);
+    //         if(response) {
+    //             const userDocRef = await createUserDocumentFromAuth(response.user);
+    //         }
+    //     }
+    
+    //     fetchData();
+    // }, []);
+    
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        console.log(user);        
         const userDocRef = await createUserDocumentFromAuth(user);
     };
 
+    
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
             </button>
+            {/* <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
+            </button> */}
+            <SignUpForm />
         </div>
     )
 };
