69e4c424fc1d873ad46f30219aa396bce061b45d
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2023-09-01 22:21:30 -0400

    created authentication page component and sign in form

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import FormInput from '../form-input/form-input.component';
+import './sign-in-form.styles.scss';
+
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    signInUserWithAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
+import Button from "../button/button.component";
+
+const defaultFormFields = {
+    email: '',
+    password: '',
+};
+
+const SignInForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields); // Use [ ] instead of { }
+
+    const { email, password } = formFields;
+
+    const resetForms = () => {
+        setFormFields(defaultFormFields);
+    };
+
+    const signInWithGoogle = async () => {
+        const { user } = await signInWithGooglePopup();
+        await createUserDocumentFromAuth();
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await signInUserWithAuthUserWithEmailAndPassword(email, password);
+            console.log(response);
+            resetForms();
+        } catch (error) {
+            console.error('User creation encountered an error', error);
+        }
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target; // Access event.target
+        setFormFields({ ...formFields, [name]: value });
+    };
+
+    return (
+        <div className="sign-up-container">
+            <h2>Already have an account?</h2>
+            <span>Sign in with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput label="Email" type="email" onChange={handleChange} name="email" value={email} required />
+                <FormInput label="Password" type="password" onChange={handleChange} name="password" value={password} required />
+                <div className="buttons-container">
+                    <Button type="sign-in" onClick={handleSubmit}>Sign In</Button>
+                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google sign In</Button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+
+export default SignInForm;
---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,21 @@
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
+import './authentication.styles.scss';
+
+import { 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils'
+
+const Authentication = () => {
+
+    return (
+        <div className='authentication-container'>
+            <SignInForm />
+            <SignUpForm />
+        </div>
+    )
+};
+
+export default Authentication;
